70c057e0bc4a900385471f06dceb953708beea82
makhkets <makhkets@gmail.com>
2025-09-21 00:45:02 +0300

    .

--- 1.11/main.go
+++ 1.11/main.go
@@ -1,1 +1,31 @@
-package __11
+package main
+
+import "fmt"
+
+// Реализовать пересечение двух неупорядоченных множеств (например, двух слайсов) — т.е.
+// вывести элементы, присутствующие и в первом, и во втором.
+
+// Пример:
+// A = {1,2,3}
+// B = {2,3,4}
+// Пересечение = {2,3}
+
+func main() {
+	A := []int{1, 2, 3}
+	B := []int{2, 3, 4}
+
+	hMap := map[int]bool{}
+	result := []int{}
+
+	for _, v := range A {
+		hMap[v] = true
+	}
+
+	for _, v := range B {
+		if hMap[v] {
+			result = append(result, v)
+		}
+	}
+
+	fmt.Println("результат", result)
+}
--- 1.12/main.go
+++ 1.12/main.go
@@ -1,1 +1,27 @@
-package __12
+package main
+
+import "fmt"
+
+// # Собственное множество строк
+
+// Имеется последовательность строк: ("cat", "cat", "dog", "cat", "tree").
+// Создать для неё собственное множество.
+
+// Ожидается: получить набор уникальных слов. Для примера, множество = {"cat", "dog", "tree"}.
+
+func main() {
+	words := []string{"cat", "cat", "dog", "cat", "tree"}
+	hmap := map[string]bool{}
+	res := []string{}
+
+	for i := 0; i < len(words); i++ {
+		if hmap[words[i]] {
+			continue
+		} else {
+			hmap[words[i]] = true
+			res = append(res, words[i])
+		}
+	}
+
+	fmt.Println(res)
+}
